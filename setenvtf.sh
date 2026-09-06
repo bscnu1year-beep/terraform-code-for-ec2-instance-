0a21f839f997248e471d90ec88660ab1e9acf52f
dhruv p <bscnu1year@gmail.com>
2026-09-06 22:20:17 +0530

    setenvtfec2
    This script updates system packages, installs prerequisites, and sets up Terraform and AWS CLI v2.

---
+++ setenvtf.sh
@@ -0,0 +1,12 @@
+# Update system packages and install prerequisites
+sudo apt-get update -y && sudo apt-get install -y gnupg software-properties-common curl unzip
+
+# Install HashiCorp Terraform
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt-get update -y && sudo apt-get install -y terraform
+
+# Install AWS CLI v2
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
