eac6ea01e43dcfd4504d15140070efc27f2ec37e
dhruv p <bscnu1year@gmail.com>
2026-09-07 11:24:34 +0530

    Update basicenv.sh

--- basicenv.sh
+++ basicenv.sh
@@ -1,3 +1,22 @@
+#!/bin/bash
+set -e
+
+# 1. Update system packages and install git
+apt-get update -y
+apt-get install -y git
+
+# 2. Navigate directly to the Ubuntu user's home directory
+cd /home/ubuntu
+
+# 3. Clone your GitHub repository
 git clone https://github.com/bscnu1year-beep/terraform-code-for-ec2-instance-.git
+
+# 4. Navigate into the cloned repository
 cd terraform-code-for-ec2-instance-
-./deployment.sh 
+
+# 5. Make deployment.sh executable and run it
+chmod +x deployment.sh
+./deployment.sh
+
+# 6. Change ownership back to the 'ubuntu' user so you can access it easily without sudo
+chown -R ubuntu:ubuntu /home/ubuntu/terraform-code-for-ec2-instance-
